aa62e06f2f2cb8ad23e6140c08fa859728ab1db2
sc302 <ibrahim.can818@gmail.com>
2019-05-28 18:53:56 +0300

    ajax

---
+++ JsonResultKullanimi/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using JsonResultKullanimi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JsonResultKullanimi.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public JsonResult GetStudent(int id)
+        {
+            //data base den de getirebilir.
+            List<Student> ogrenciler = new List<Student>();
+            ogrenciler.Add(new Student()
+            {
+                StudentId = 1,
+                StudentName = "Gülsüm",
+                StudentLastName = "Solhan",
+                Avarage = 4
+            });
+            ogrenciler.Add(new Student()
+            {
+                StudentId = 2,
+                StudentName = "Tugce",
+                StudentLastName = "Kokcu",
+                Avarage = 5
+            });
+            ogrenciler.Add(new Student()
+            {
+                StudentId = 3,
+                StudentName = "Ziya",
+                StudentLastName = "Oses",
+                Avarage = 9
+            });
+
+           var ogrenci=  ogrenciler.FirstOrDefault(x => x.StudentId == id);
+
+            return Json(ogrenci, JsonRequestBehavior.AllowGet);
+
+        }
+    }
+}
---
+++ JsonResultKullanimi/Models/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace JsonResultKullanimi.Models
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+        public string StudentName { get; set; }
+        public string StudentLastName { get; set; }
+        public int Avarage { get; set; }
+    }
+}
